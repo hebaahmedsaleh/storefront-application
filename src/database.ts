c7bcb1f35e2d58ab9e332db19d4a503a78246f4d
hebaahmed400@gmail.com <hebaahmed400@gmail.com>
2022-11-05 01:33:23 +0200

    add database file

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,17 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-import { Pool } from 'pg'
+import { Pool } from 'pg';
 
-dotenv.config()
+dotenv.config();
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_TEST_DB, POSTGRES_USER, POSTGRES_PASSWORD, ENV } =
+  process.env;
 
 const client = new Pool({
   host: POSTGRES_HOST,
-  database: POSTGRES_DB,
+  database: ENV === 'dev' ? POSTGRES_DB : POSTGRES_TEST_DB,
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD
-})
+});
 
-export default client
+export default client;
